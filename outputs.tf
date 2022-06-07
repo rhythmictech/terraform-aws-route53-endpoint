29a7f38ad940a708daa13821dce9768cff189b8e
Mark Khateeb <m.khateeb15@gmail.com>
2022-06-07 10:42:58 +0100

    tf1.2.1_upgrade

--- outputs.tf
+++ outputs.tf
@@ -10,5 +10,5 @@ output "security_group_id" {
 
 output "ip_addresses" {
   description = "Resolver IP addresses"
-  value = [for obj in aws_route53_resolver_endpoint.resolver_endpoint.ip_address : obj.ip]
+  value       = [for obj in aws_route53_resolver_endpoint.resolver_endpoint.ip_address : obj.ip]
 }
